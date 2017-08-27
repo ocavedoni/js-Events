a7a25ba7600fd06a0747400bd41a2279c0b65c57
Olina Cavedoni <Olina@Olinas-MacBook-Pro.local>
2017-08-26 17:09:19 -1000

    trying again

--- js/app.js
+++ js/app.js
@@ -6,12 +6,26 @@ The function will add the following content inside the paragraph with the id of 
 
 var bacon = "Shoulder turducken brisket, kevin swine andouille tri-tip salami tail ham sausage pork loin. Ribeye short loin rump kielbasa pork. Capicola short loin turducken corned beef tongue, chuck leberkas salami frankfurter. Kielbasa fatback pancetta, ground round meatball turducken jowl ribeye alcatra sirloin bacon corned beef beef ribs short loin. Pork belly spare ribs biltong corned beef meatball short ribs tongue alcatra swine drumstick. Biltong shankle kevin, cupim sirloin bresaola brisket. Tail pork belly biltong ball tip tri-tip, pig jerky cow pastrami prosciutto ;ground round bacon capicola tongue meatball.";
 
+var clickMsg = document.getElementById("one");
+click.Msg.addEventListener("click", moreContent);
+
+function moreContent(){
+	var showMore = document.getElementById("more");
+	showMore.innerHTML = bacon;
+}
+
 
 //2. HTTP
 /*Create a function named `lessContent` that will initiate the `Show Less` link after clicking on it.
 
 The function will hide the contents in the pargraph with the id of `less` after clicking on the `Show Less` link.*/
 
+var clickMsg2 = document.getElementById();
+
+function lessContent(){
+	var showLess = document.getElementById("less");
+	showLess.innerHTML = "";
+}
 
 //3. Tacocat, The Original Palindrome King
 /*Create a function named `zoom` that will increase the font size of the paragraph with the id of `biggie` after hovering your mouse over it. Increae the font size to 150%*/
